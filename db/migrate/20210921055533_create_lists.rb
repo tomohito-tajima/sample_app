05801d3abc7c0305c1e1d0cbdd228347292636c4
EC2 Default User <ec2-user@ip-172-31-68-142.ec2.internal>
2021-10-06 02:59:01 +0000

    Finish2-4

--- db/migrate/20210921055533_create_lists.rb
+++ db/migrate/20210921055533_create_lists.rb
@@ -3,7 +3,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
     create_table :lists do |t|
       # 以下の2行を追加
       t.string :title
-      t.string :
+      t.string :body
 
       t.timestamps
     end
